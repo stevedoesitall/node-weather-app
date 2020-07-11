e957431a2a477a893f4dfe849c967a12725660f5
Steve Giordano <stephenagiordano@gmail.com>
2020-07-11 19:42:35 -0400

    Fixed callback error

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,15 +4,19 @@ const forecast = async (lat, long, callback) => {
     const accessKey = '41ae81407b1a44c566fc8f33f1df12f8'
     const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=-${lat},${long}&units=f`
     const response = await fetch(url)
-    if (response.status === 200) {
-        const data = await response.json()
-        if (!data.error) {
-            callback(undefined, data.current.temperature)
+    try {
+        if (response.status === 200) {
+            const data = await response.json()
+            if (!data.error) {
+                callback(undefined, data.current.temperature)
+            } else {
+                throw new Error('Error: No data')
+            }
         } else {
-            callback('Something went wrong!', undefined)
+            throw new Error('Error: Bad request')
         }
-    } else {
-        callback('Something went wrong!', undefined)
+    } catch (error) {
+        callback(error)
     }
 }
 
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -5,20 +5,25 @@ const geocode = async (address, callback) => {
   const mapApiKey = 'pk.eyJ1Ijoic3RldmVkb2VzaXRhbGwiLCJhIjoiY2tjZXJjYXh3MDRwOTJ5bnF1Ymxqdm5uYyJ9.OLkkWVz9Xskqni-8Hgr9IA'
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapApiKey}&limit=1`
   const response = await fetch(url)
-  if (response.status === 200) {
-      const data = await response.json()
-      if (!data.error) {
-          const features = {
-            latitude: data.features[0].center[1],
-            longitude: data.features[0].center[0],
-            location: data.features[0].place_name
-          }
-          callback(undefined, features)
-      } else {
-          callback('Something went wrong!', undefined)
-      }
-  } else {
-      callback('Something went wrong!', undefined)
+  
+  try {
+    if (response.status === 200) {
+        const data = await response.json()
+        if (!data.error) {
+            const features = {
+              latitude: data.features[0].center[1],
+              longitude: data.features[0].center[0],
+              location: data.features[0].place_name
+            }
+            callback(undefined, features)
+        } else {
+          throw new Error('Error: No data')
+        }
+    } else {
+      throw new Error('Error: Bad request')
+    }
+  } catch (error) {
+    callback(error)
   }
 }
 
